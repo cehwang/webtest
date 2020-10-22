d02796996677f76b25068e479ab5fa0305e523c2
cehwang <hwangwhang.c@gmail.com>
2020-10-22 22:17:57 +0900

    update testcase url

--- gspreadAPI.py
+++ gspreadAPI.py
@@ -12,7 +12,7 @@ JSON_FILE_NAME = "ferrous-ranger-280410-a01265739651.json"
 CREDENTIAL = ServiceAccountCredentials.from_json_keyfile_name(JSON_FILE_NAME, SCOPE)
 GC = gspread.authorize(CREDENTIAL)
 
-TESTCASE_CARD_URL = "https://docs.google.com/spreadsheets/d/1tAaE06aP_Bk5UuVsI-wKvoZTOywf-GYz2WY8NseYuTw/"
-TESTCASE_CMA_URL = "https://docs.google.com/spreadsheets/d/1tAaE06aP_Bk5UuVsI-wKvoZTOywf-GYz2WY8NseYuTw/"
+TESTCASE_CARD_URL = "https://docs.google.com/spreadsheets/d/1x2K17s572840AHiroltglV_nwLPwcLonVxB8kZ07FcM/edit#gid=1836599704"
+TESTCASE_CMA_URL = "https://docs.google.com/spreadsheets/d/1x2K17s572840AHiroltglV_nwLPwcLonVxB8kZ07FcM/edit#gid=1836599704"
 TESTCASE_LOAN_URL = "https://docs.google.com/spreadsheets/d/1tAaE06aP_Bk5UuVsI-wKvoZTOywf-GYz2WY8NseYuTw/edit#gid=445109408"
 TESTCASE_CAL_URL = "https://docs.google.com/spreadsheets/d/1tAaE06aP_Bk5UuVsI-wKvoZTOywf-GYz2WY8NseYuTw/edit#gid=445109408"
